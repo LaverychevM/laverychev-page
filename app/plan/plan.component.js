6deb12b16cfd958446ccc7c4a61706ebc00a741b
LaverychevM <55694988+LaverychevM@users.noreply.github.com>
2020-02-19 22:04:57 +0300

    Update and rename plan.js to plan.component.js

---
+++ app/plan/plan.component.js
@@ -0,0 +1,9 @@
+angular.
+    module('designApp').
+    component('about', {
+        templateUrl: 'app/about/about.template.html',
+        controller: function AboutController() {
+
+        }
+    });
+© 2020 GitHub, Inc.
